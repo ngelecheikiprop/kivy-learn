e1b4ba85ac4b528e2b44d83dc3adcfb0ad70c4d3
ngelecheikiprop <ngelecheikiprop@gmail.com>
2025-01-25 05:51:33 +0300

    add a font

--- hello-kivy.py
+++ hello-kivy.py
@@ -5,7 +5,7 @@ from kivy.uix.label import Label
 
 class MyApp(App):
     def build(self):
-        return Label(text="hello world")
+        return Label(text="hello world", font_size=72)
 
 if __name__=='__main__':
     MyApp().run()
